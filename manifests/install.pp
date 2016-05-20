b8accebb2c1dafd905386adf94dfb9ca87141506
Frédéric DROUET <fdrouet@exoplatform.com>
2016-05-20 14:55:26 +0200

    Use official apt module instead of eXo repo module

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,10 @@
 class apparmor::install {
-  package { $apparmor::params::package_name:
-    ensure  => $apparmor::installed ? {
+  ensure_packages ( $apparmor::params::package_name, {
+    'ensure'  => $apparmor::installed ? {
       true    => present,
       default => absent,
     },
-    require => [
-      Exec['repo-update'],],
-    notify  => Class['apparmor::config'],
-  }
+    'require' => Class['apt::update'],
+    'notify'  => Class['apparmor::config'],
+  })
 }
